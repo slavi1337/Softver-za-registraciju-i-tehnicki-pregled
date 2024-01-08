07f0cabd20911a2e33d8c6cf490f448fe7bbf19d
Slaviša Čovakušić <slavisacovakusic@gmail.com>
2024-01-08 02:40:46 +0100

    Solved: Issue #31 , Issue #27 fixed and tested #24 #25 #26 stavio sve u jedan fajl normalno

--- SoftverZaRegistraciju/SoftverZaRegistraciju/AdminForm.Designer.cs
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/AdminForm.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminForm";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            button4 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(217, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 83);
+            button1.TabIndex = 0;
+            button1.Text = "Kreiraj nalog";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(217, 213);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 80);
+            button2.TabIndex = 1;
+            button2.Text = "Brisi nalog";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 455);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 36);
+            button3.TabIndex = 2;
+            button3.Text = "Odjava";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(347, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(723, 479);
+            dataGridView1.TabIndex = 3;
+            dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(217, 416);
+            button4.Name = "button4";
+            button4.Size = new Size(124, 75);
+            button4.TabIndex = 4;
+            button4.Text = "Osvjezi";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(12, 377);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 57);
+            button5.TabIndex = 5;
+            button5.Text = "Promijeni sifru";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // AdminForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(1082, 503);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "AdminForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Admin Panel";
+            Load += AdminForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView dataGridView1;
+        private Button button4;
+        private Button button5;
     }
 }

6adc065746dbe96e8cdc194dd59e4071a5ed8f92
Slaviša Čovakušić <73200412+slavi1337@users.noreply.github.com>
2024-01-11 04:31:32 +0100

    Solved: Issue #28
    Pregled vlastitih registracija, ovo se nalazi na formi za korisnika

--- SoftverZaRegistraciju/SoftverZaRegistraciju/UserForm.Designer.cs
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/UserForm.Designer.cs
@@ -28,12 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserForm";
+            button1 = new Button();
+            btnCalc = new Button();
+            btnNotif = new Button();
+            button5 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            label3 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(472, 43);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 68);
+            button1.TabIndex = 15;
+            button1.Text = "Istorija kazni";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // btnCalc
+            // 
+            btnCalc.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCalc.ForeColor = SystemColors.Highlight;
+            btnCalc.Location = new Point(688, 43);
+            btnCalc.Margin = new Padding(4, 3, 4, 3);
+            btnCalc.Name = "btnCalc";
+            btnCalc.Size = new Size(146, 68);
+            btnCalc.TabIndex = 14;
+            btnCalc.Text = "Kalkulator registracije";
+            btnCalc.UseVisualStyleBackColor = true;
+            btnCalc.Click += btnCalc_Click;
+            // 
+            // btnNotif
+            // 
+            btnNotif.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnNotif.ForeColor = SystemColors.Highlight;
+            btnNotif.Location = new Point(904, 43);
+            btnNotif.Margin = new Padding(4, 3, 4, 3);
+            btnNotif.Name = "btnNotif";
+            btnNotif.Size = new Size(146, 68);
+            btnNotif.TabIndex = 13;
+            btnNotif.Text = "Podesavanje obavjestenja";
+            btnNotif.UseVisualStyleBackColor = true;
+            btnNotif.Click += btnNotif_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.ForeColor = SystemColors.Highlight;
+            button5.Location = new Point(13, 18);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 57);
+            button5.TabIndex = 17;
+            button5.Text = "Promijeni sifru";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.Highlight;
+            button3.Location = new Point(13, 99);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 36);
+            button3.TabIndex = 16;
+            button3.Text = "Odjava";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(237, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(558, 34);
+            label1.TabIndex = 19;
+            label1.Text = "Informacije o vlastitim registracijama";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 55);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(1058, 268);
+            dataGridView1.TabIndex = 20;
+            dataGridView1.DataBindingComplete += dataGridView1_DataBindingComplete;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Info;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(btnCalc);
+            panel1.Controls.Add(btnNotif);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button5);
+            panel1.Location = new Point(-1, 356);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1086, 152);
+            panel1.TabIndex = 21;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(260, 18);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 20);
+            label3.TabIndex = 19;
+            label3.Text = "label3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(126, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(137, 20);
+            label2.TabIndex = 18;
+            label2.Text = "Korisnicko ime:";
+            // 
+            // UserForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(1082, 503);
+            Controls.Add(panel1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "UserForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Korisnicki Panel";
+            FormClosing += UserForm_FormClosing;
+            Load += UserForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button btnCalc;
+        private Button btnNotif;
+        private Button button5;
+        private Button button3;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Label label2;
+        private Label label3;
     }
 }
--- SoftverZaRegistraciju/SoftverZaRegistraciju/UserForm.cs
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/UserForm.cs
@@ -1,20 +1,243 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace SoftverZaRegistraciju
 {
+
     public partial class UserForm : Form
     {
-        public UserForm()
+        private string loggedInUsername;
+        private CalcForm? calcForm = null;
+        private changePass? passChangeForm = null;
+        private IstorijaKazni? istorijaKazniForm = null;
+        private PostavkeObavjestenja? postavkeObavjestenjaForm = null;
+        private readonly string connectionString = "Data Source=DESKTOP-0THU24A\\SQLEXPRESS;Initial Catalog=KorisnickiNalozi;Integrated Security=True";
+
+        public UserForm(string username)
         {
             InitializeComponent();
+            loggedInUsername = username;
+            dataGridView1.DataBindingComplete += new DataGridViewBindingCompleteEventHandler(dataGridView1_DataBindingComplete);
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (calcForm == null || calcForm.IsDisposed)
+            {
+                calcForm = new CalcForm();
+                calcForm.FormClosed += (s, args) => calcForm = null;
+                calcForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Vec imate otvorenu formu za kalkulator.\nZatvorite je, pa je opet mozete pokrenuti.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (passChangeForm == null || passChangeForm.IsDisposed)
+            {
+                passChangeForm = new changePass(loggedInUsername);
+                passChangeForm.FormClosed += (s, args) => passChangeForm = null;
+                passChangeForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Vec imate otvorenu formu za mijenjanje sifre.\nZatvorite je, pa je opet mozete pokrenuti.");
+            }
+        }
+
+        //odjava
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CloseSecondaryForms();
+            this.Hide();
+            Form1 loginForm = new Form1();
+            loginForm.Closed += (s, args) => this.Close();
+            loginForm.Show();
+        }
+
+        private DataTable GetData(string query)
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Username", loggedInUsername);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dataTable);
+                }
+            }
+            return dataTable;
+        }
+
+        private void UserForm_Load(object sender, EventArgs e)
+        {
+            label3.Text = loggedInUsername;
+            string queryRegistrovanaVozila = "SELECT BrSasije, RegistrationDate, ExpiryDate, RegTablice FROM RegistrovanaVozila WHERE JMBG = (SELECT JMBG FROM ObicniKorisnickiNalozi WHERE Username = @Username)";
+            string queryVozilaInformacije = "SELECT BrSasije, ImeVozila, Boja, GodinaProizvodnje, JacinaMotora, ZapreminaMotora FROM VozilaInformacije";
+
+            var registrovanaVozilaData = GetData(queryRegistrovanaVozila);
+            var vozilaInformacijeData = GetData(queryVozilaInformacije);
+
+            var spojeniPodaci = from rv in registrovanaVozilaData.AsEnumerable()
+                                join vi in vozilaInformacijeData.AsEnumerable()
+                                on rv["BrSasije"] equals vi["BrSasije"]
+                                select new
+                                {
+                                    BrSasije = rv["BrSasije"],
+                                    ImeVozila = vi["ImeVozila"],
+                                    Boja = vi["Boja"],
+                                    GodinaProizvodnje = vi["GodinaProizvodnje"],
+                                    JacinaMotora = vi["JacinaMotora"],
+                                    ZapreminaMotora = vi["ZapreminaMotora"],
+                                    DatumIstekaRegistracije = rv["ExpiryDate"],
+                                    RegistarskeTablice = rv["RegTablice"]
+                                };
+
+            dataGridView1.DataSource = spojeniPodaci.ToList();
+            SendRegistrationNotifications();
+        }
+
+        private void SendRegistrationNotifications()
+        {
+            int notificationDays = GetNotificationDays(loggedInUsername);
+            DateTime today = DateTime.Today;
+
+            // za svaku registraciju se provjerava da li se treba slati notifikacija
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DateTime expiryDate = Convert.ToDateTime(row.Cells["DatumIstekaRegistracije"].Value);
+                string vehicleName = row.Cells["ImeVozila"].Value?.ToString() ?? "Nepoznato vozilo";
+                int daysUntilExpiry = (expiryDate - today).Days;
+
+                if (daysUntilExpiry <= notificationDays)
+                {
+                    MessageBox.Show($"Obavjestenje: Registracija za vozilo '{vehicleName}' istice za {daysUntilExpiry} dana.");
+                }
+            }
+        }
+
+        private int GetNotificationDays(string username)
+        {
+            int notificationDays = 0;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "SELECT Broj FROM ObavjestenjaPostavke WHERE Username = @Username";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+
+                    object result = cmd.ExecuteScalar();
+                    if (result != null)
+                    {
+                        notificationDays = Convert.ToInt32(result);
+                    }
+                }
+            }
+            return notificationDays;
+        }
+
+        private void dataGridView1_DataBindingComplete(object? sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+                if (col.Name == "BrSasije")
+                {
+                    col.HeaderText = "Broj Sasije";
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    col.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Regular);
+                }
+                else
+                {
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    col.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Regular);
+                }
+
+                switch (col.Name)
+                {
+                    case "ImeVozila":
+                        col.HeaderText = "Vozilo";
+                        break;
+                    case "GodinaProizvodnje":
+                        col.HeaderText = "Godina Proizvodnje";
+                        break;
+                    case "JacinaMotora":
+                        col.HeaderText = "Jacina Motora";
+                        break;
+                    case "ZapreminaMotora":
+                        col.HeaderText = "Zapremina Motora";
+                        break;
+                    case "DatumIstekaRegistracije":
+                        col.HeaderText = "Datum Isteka Reg.";
+                        col.DefaultCellStyle.Format = "dd.MM.yyyy";
+                        break;
+                    case "RegistarskeTablice":
+                        col.HeaderText = "Tablice";
+                        break;
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (istorijaKazniForm == null || istorijaKazniForm.IsDisposed)
+            {
+                istorijaKazniForm = new IstorijaKazni(loggedInUsername);
+                istorijaKazniForm.FormClosed += (s, args) => istorijaKazniForm = null;
+                istorijaKazniForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Vec imate otvorenu formu za istoriju kaznu.\nZatvorite je, pa je opet mozete pokrenuti.");
+            }
+        }
+
+        private void btnNotif_Click(object sender, EventArgs e)
+        {
+            if (postavkeObavjestenjaForm == null || postavkeObavjestenjaForm.IsDisposed)
+            {
+                postavkeObavjestenjaForm = new PostavkeObavjestenja(loggedInUsername);
+                postavkeObavjestenjaForm.FormClosed += (s, args) => postavkeObavjestenjaForm = null;
+                postavkeObavjestenjaForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Vec imate otvorenu formu za postavke obavjestenja.\nZatvorite je, pa je opet mozete pokrenuti.");
+            }
+        }
+
+        private void CloseSecondaryForms()
+        {
+            calcForm?.Close();
+            passChangeForm?.Close();
+            istorijaKazniForm?.Close();
+            postavkeObavjestenjaForm?.Close();
+        }
+
+        private void UserForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if ((calcForm != null && !calcForm.IsDisposed) ||
+            (passChangeForm != null && !passChangeForm.IsDisposed) ||
+            (istorijaKazniForm != null && !istorijaKazniForm.IsDisposed) ||
+            (postavkeObavjestenjaForm != null && !postavkeObavjestenjaForm.IsDisposed))
+            {
+                MessageBox.Show("Molimo zatvorite sve otvorene podforme prije nego sto zatvorite ovu formu.");
+                e.Cancel = true;
+            }
+            else
+            {
+                Application.Exit();
+            }
         }
     }
 }

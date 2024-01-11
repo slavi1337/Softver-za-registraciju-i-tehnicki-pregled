c5a010609363d863a51670eca529737eeae916d8
Slaviša Čovakušić <73200412+slavi1337@users.noreply.github.com>
2024-01-11 04:22:36 +0100

    Forma za Issue #25
    Adminu se omogucava da kreira nalog za radnika/admina.

---
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/CreateAccount.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace SoftverZaRegistraciju
+{
+    partial class CreateAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            checkBox1 = new CheckBox();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(52, 118);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Korisnicko ime";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(123, 191);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Lozinka";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 260);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(159, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Potvrda lozinke";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(217, 371);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(105, 27);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Admina";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(217, 404);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(110, 27);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Radnika";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(191, 471);
+            button1.Name = "button1";
+            button1.Size = new Size(177, 62);
+            button1.TabIndex = 5;
+            button1.Text = "Kreiraj nalog";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(217, 110);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(255, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(217, 183);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(255, 31);
+            textBox2.TabIndex = 7;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(217, 252);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(255, 31);
+            textBox3.TabIndex = 8;
+            textBox3.UseSystemPasswordChar = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(303, 316);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(169, 27);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "Prikazi lozinku";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(80, 385);
+            label4.Name = "label4";
+            label4.Size = new Size(116, 23);
+            label4.TabIndex = 10;
+            label4.Text = "Nalog je za";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Rounded MT Bold", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(80, 28);
+            label5.Name = "label5";
+            label5.Size = new Size(432, 34);
+            label5.TabIndex = 11;
+            label5.Text = "Unesite informacije o nalogu";
+            // 
+            // CreateAccount
+            // 
+            AutoScaleDimensions = new SizeF(12F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(546, 579);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "CreateAccount";
+            Text = "Admin Panel - Kreiranje naloga";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private CheckBox checkBox1;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/CreateAccount.cs
@@ -0,0 +1,84 @@
+﻿using System.Data.SqlClient;
+
+namespace SoftverZaRegistraciju
+{
+    public partial class CreateAccount : Form
+    {
+        private readonly string connectionString = "Data Source=DESKTOP-0THU24A\\SQLEXPRESS;Initial Catalog=KorisnickiNalozi;Integrated Security=True";
+
+        public CreateAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = !checkBox1.Checked;
+            textBox3.UseSystemPasswordChar = !checkBox1.Checked;
+        }
+
+        private bool UsernameExists(string username)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM KorisnickiNalozi WHERE Username = @username", con))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    int userCount = (int)cmd.ExecuteScalar();
+                    return (userCount > 0);
+                }
+            }
+        }
+
+        private bool CreateOneAccount(string username, string password, int role)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO KorisnickiNalozi (Username, Password, Role) VALUES (@username, @password, @role)", con))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    cmd.Parameters.AddWithValue("@role", role);
+                    return (cmd.ExecuteNonQuery() == 1);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            string confirmPassword = textBox3.Text;
+
+            // Provjera da li se lozinke podudaraju
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Lozinke se ne podudaraju.");
+                return;
+            }
+
+            // Provjera da li korisnicko ime vec postoji
+            if (UsernameExists(username))
+            {
+                MessageBox.Show("Korisnicko ime vec postoji.");
+                return;
+            }
+
+            // Odredjivanje role-a na osnovu izabranog radio buttona
+            int role = radioButton1.Checked ? 2 : 1;
+
+            // Kreiranje naloga
+            if (CreateOneAccount(username, password, role))
+            {
+                MessageBox.Show("Nalog je uspjesno kreiran.");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Doslo je do greske prilikom kreiranja naloga.");
+            }
+        }
+    }
+}

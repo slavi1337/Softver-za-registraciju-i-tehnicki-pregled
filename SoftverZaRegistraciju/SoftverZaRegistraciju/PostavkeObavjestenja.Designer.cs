4415fc1f5e5ad70e80890d175b4d52f2415c9eb5
Slaviša Čovakušić <73200412+slavi1337@users.noreply.github.com>
2024-01-11 04:29:28 +0100

    Solved: Issue #29
    Podesavanje obavjestenja o isteku registracije vlastitog vozila.

---
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/PostavkeObavjestenja.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SoftverZaRegistraciju
+{
+    partial class PostavkeObavjestenja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PostavkeObavjestenja));
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = SystemColors.ControlLight;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "0", "3", "5", "7", "10", "15" });
+            comboBox1.Location = new Point(160, 81);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 31);
+            comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(42, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(400, 40);
+            label1.TabIndex = 1;
+            label1.Text = "Izaberite na koliko dana prije isteka registracije\r\nzelite da primate obavjestenja";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLight;
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(160, 226);
+            button1.Name = "button1";
+            button1.Size = new Size(151, 62);
+            button1.TabIndex = 2;
+            button1.Text = "Sacuvaj postavke";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(5, 357);
+            label2.Name = "label2";
+            label2.Size = new Size(437, 165);
+            label2.TabIndex = 3;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // PostavkeObavjestenja
+            // 
+            AutoScaleDimensions = new SizeF(12F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(494, 527);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "PostavkeObavjestenja";
+            Text = "Korisnicki Panel - Podesavanje Obavjestenja";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+    }
+}
---
+++ SoftverZaRegistraciju/SoftverZaRegistraciju/PostavkeObavjestenja.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.SqlClient;
+
+namespace SoftverZaRegistraciju
+{
+    public partial class PostavkeObavjestenja : Form
+    {
+        private string loggedInUsername;
+        private readonly string connectionString = "Data Source=DESKTOP-0THU24A\\SQLEXPRESS;Initial Catalog=KorisnickiNalozi;Integrated Security=True";
+
+        public PostavkeObavjestenja(string username)
+        {
+            InitializeComponent();
+            loggedInUsername = username;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int notificationDays = Convert.ToInt32(comboBox1.SelectedItem);
+
+            SaveNotificationSettings(loggedInUsername, notificationDays);
+            MessageBox.Show("Uspjesno ste sacuvali postavke obavjestenja");
+            this.Close();
+        }
+
+        private void SaveNotificationSettings(string username, int days)
+        { 
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                // provjera da li vec postoji opcija za korisnika
+                string queryCheck = "SELECT COUNT(*) FROM ObavjestenjaPostavke WHERE Username = @Username";
+                using (SqlCommand cmdCheck = new SqlCommand(queryCheck, con))
+                {
+                    cmdCheck.Parameters.AddWithValue("@Username", username);
+                    int exists = (int)cmdCheck.ExecuteScalar();
+
+                    string query;
+                    if (exists > 0)
+                    {
+                        query = "UPDATE ObavjestenjaPostavke SET Broj = @Days WHERE Username = @Username";
+                    }
+                    else // Ako ne postoji, create novi red
+                    {
+                        query = "INSERT INTO ObavjestenjaPostavke (Username, Broj) VALUES (@Username, @Days)";
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Days", days);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+    }
+}

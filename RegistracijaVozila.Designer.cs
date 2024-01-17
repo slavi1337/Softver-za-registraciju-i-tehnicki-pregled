94c794a26754ed5cf3b930c2a0ea72e97dc8bd4a
Anja Ljiljak <153135831+AnjaLj@users.noreply.github.com>
2024-01-17 21:48:59 +0100

    Solved: Issue #36 #37
    Unos informacija o vozilu i vlasniku vozila u reg formi

---
+++ RegistracijaVozila.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace SoftverZaRegistraciju
+{
+    partial class RegistracijaVozila
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            button5 = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(556, 250);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(234, 31);
+            textBox6.TabIndex = 23;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(556, 213);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(234, 31);
+            textBox5.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(556, 176);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(234, 31);
+            textBox4.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(556, 139);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(234, 31);
+            textBox3.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(556, 102);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(234, 31);
+            textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(556, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(234, 31);
+            textBox1.TabIndex = 18;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(260, 258);
+            label6.Name = "label6";
+            label6.Size = new Size(252, 23);
+            label6.TabIndex = 17;
+            label6.Text = "Zapremina motora [ccm]";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(260, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(199, 23);
+            label5.TabIndex = 16;
+            label5.Text = "Godina proizvodnje";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(260, 184);
+            label4.Name = "label4";
+            label4.Size = new Size(198, 23);
+            label4.TabIndex = 15;
+            label4.Text = "Jacina motora [kW]";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(260, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 23);
+            label3.TabIndex = 14;
+            label3.Text = "Boja";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(260, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 23);
+            label2.TabIndex = 13;
+            label2.Text = "Naziv";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(260, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 23);
+            label1.TabIndex = 12;
+            label1.Text = "Broj sasije";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(556, 438);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(234, 31);
+            textBox7.TabIndex = 29;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(556, 401);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(234, 31);
+            textBox8.TabIndex = 28;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(556, 364);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(234, 31);
+            textBox9.TabIndex = 27;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(260, 446);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 23);
+            label7.TabIndex = 26;
+            label7.Text = "JMBG";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(260, 409);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 23);
+            label8.TabIndex = 25;
+            label8.Text = "Prezime";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(260, 372);
+            label9.Name = "label9";
+            label9.Size = new Size(51, 23);
+            label9.TabIndex = 24;
+            label9.Text = "Ime ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(375, 19);
+            label10.Name = "label10";
+            label10.Size = new Size(281, 32);
+            label10.TabIndex = 30;
+            label10.Text = "Informacije o vozilu";
+            label10.Click += label10_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.Location = new Point(312, 317);
+            label11.Name = "label11";
+            label11.Size = new Size(401, 32);
+            label11.TabIndex = 31;
+            label11.Text = "Informacije o vlasniku vozila";
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ControlLight;
+            button5.ForeColor = SystemColors.Highlight;
+            button5.Location = new Point(12, 446);
+            button5.Name = "button5";
+            button5.Size = new Size(100, 46);
+            button5.TabIndex = 35;
+            button5.Text = "Nazad";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(890, 65);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 87);
+            button1.TabIndex = 36;
+            button1.Text = "Registruj vozilo";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // RegistracijaVozila
+            // 
+            AutoScaleDimensions = new SizeF(12F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(1082, 503);
+            Controls.Add(button1);
+            Controls.Add(button5);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(textBox7);
+            Controls.Add(textBox8);
+            Controls.Add(textBox9);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "RegistracijaVozila";
+            Text = "Radnicki Panel - Registracija Vozila";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Button button5;
+        private Button button1;
+    }
+}
---
+++ RegistracijaVozila.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftverZaRegistraciju
+{
+    public partial class RegistracijaVozila : Form
+    {
+        public RegistracijaVozila()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
